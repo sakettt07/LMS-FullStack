ab52ef3282ee922b41b6808fe039ae8a086b96a1
saket <saket@saketgupta.com>
2025-04-22 23:47:00 +0530

    started with the books add controller integrating the cloudinary support for bookCover image

--- LibraryManagementFullStack/Backend/Controllers/book.controller.js
+++ LibraryManagementFullStack/Backend/Controllers/book.controller.js
@@ -2,7 +2,12 @@ import { catchAsyncErrors } from '../Middlewares/catchAsyncErrors.js';
 import ErrorHandler from '../Middlewares/error.Middleware.js';
 import { Book } from '../Models/book.models.js';
 
-const addBook = catchAsyncErrors(async (req, res, next) => {});
+const addBook = catchAsyncErrors(async (req, res, next) => {
+    const {title,author,description,price,quantity,category} = req.body;
+    if(!title || !author || !description || !price || !quantity || !category){
+        return next(new ErrorHandler("Please enter all fields",400));
+    }
+});
 
 const getAllBooks = catchAsyncErrors(async (req, res, next) => {});
 
