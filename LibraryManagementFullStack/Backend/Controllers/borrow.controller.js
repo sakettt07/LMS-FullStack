22194fccc4cd6728cb2a6e4cbe15e1283e5eaf1d
saket <saket@saketgupta.com>
2025-04-25 23:57:39 +0530

    working on the borrow controller

--- LibraryManagementFullStack/Backend/Controllers/borrow.controller.js
+++ LibraryManagementFullStack/Backend/Controllers/borrow.controller.js
@@ -3,4 +3,14 @@ import ErrorHandler from '../Middlewares/error.Middleware.js';
 import { Borrow } from '../Models/borrow.models.js';
 import { Book } from '../Models/book.models.js';
 
-export {};
+const borrowBook = catchAsyncErrors(async (req, res, next) => {
+});
+const recordBorrowedBooks=catchAsyncErrors(async (req, res, next) => {});
+
+const getBorrowedBooksForAdmin=catchAsyncErrors(async (req, res, next) => {});
+
+const returnBorrowedBook=catchAsyncErrors(async (req, res, next) => {});
+
+
+
+export {borrowBook,recordBorrowedBooks,getBorrowedBooksForAdmin,returnBorrowedBook};
